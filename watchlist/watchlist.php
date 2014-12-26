ece4f4bc424db283e6b47d8fa4abdc4aae36c2b2
Navjot Tomer <navjottomer@gmail.com>
2014-12-26 17:42:55 +0530

    Added compatibility with tuffclassified theme

--- watchlist/watchlist.php
+++ watchlist/watchlist.php
@@ -5,12 +5,12 @@
 	}
 
     $itemsPerPage = (Params::getParam('itemsPerPage') != '') ? Params::getParam('itemsPerPage') : 5;
-    $iPage        = (Params::getParam('iPage') != '') ? Params::getParam('iPage') : 0;
+    $iPage        = (Params::getParam('iPage') != '') ? Params::getParam('iPage') : 1;
 
     Search::newInstance()->addConditions(sprintf("%st_item_watchlist.fk_i_user_id = %d", DB_TABLE_PREFIX, $i_userId));
     Search::newInstance()->addConditions(sprintf("%st_item_watchlist.fk_i_item_id = %st_item.pk_i_id", DB_TABLE_PREFIX, DB_TABLE_PREFIX));
     Search::newInstance()->addTable(sprintf("%st_item_watchlist", DB_TABLE_PREFIX));
-    Search::newInstance()->page($iPage, $itemsPerPage);
+    Search::newInstance()->page($iPage -1, $itemsPerPage);
 
     $aItems      = Search::newInstance()->doSearch();
     $iTotalItems = Search::newInstance()->count();
@@ -18,7 +18,7 @@
 
     View::newInstance()->_exportVariableToView('items', $aItems);
     View::newInstance()->_exportVariableToView('search_total_pages', $iNumPages);
-    View::newInstance()->_exportVariableToView('search_page', $iPage) ;
+    View::newInstance()->_exportVariableToView('search_page', $iPage -1) ;
 
 	// delete item from watchlist
 	function delete_item($item, $uid){
@@ -26,53 +26,40 @@
 		$conn->osc_dbExec("DELETE FROM %st_item_watchlist WHERE fk_i_item_id = %d AND fk_i_user_id = %d LIMIT 1", DB_TABLE_PREFIX , $item, $uid);
 	}
 ?>
-<div class="content user_account">
-    <h1>
-        <strong><?php _e('Your watchlist', 'watchlist'); ?></strong>
-    </h1>
-    <div id="sidebar">
-        <?php echo osc_private_user_menu(); ?>
-    </div>
-    <div id="main">
-        <?php if (osc_count_items() == 0) { ?>
-        <h3><?php _e('You don\'t have any items yet', 'watchlist'); ?></h3>
-        <?php } else { ?>
-        <h3><?php printf(_n('You are watching %d item', 'You are watching %d items', $iTotalItems, 'watchlist'), $iTotalItems) ; ?></h3>
-        <div class="ad_list">
-            <div id="list_head"></div>
-            <table border="0" cellspacing="0">
-                <tbody>
-                    <?php $class = "even"; ?>
-                    <?php while ( osc_has_items() ) { ?>
-                    <tr class="<?php echo $class; ?>">
-                        <?php if (osc_images_enabled_at_items()) { ?>
-                            <td class="photo">
-                            <?php if (osc_count_item_resources()) { ?>
-                                <a href="<?php echo osc_item_url(); ?>"><img src="<?php echo osc_resource_thumbnail_url(); ?>" width="75px" height="56px" title="" alt="" /></a>
-                            <?php } else { ?>
-                                <img src="<?php echo osc_current_web_theme_url('images/no_photo.gif'); ?>" title="" alt="" />
-                            <?php } ?>
-                            </td>
-                        <?php } ?>
-                        <td class="text">
-                            <h3>
-                                <a href="<?php echo osc_item_url(); ?>"><?php echo osc_item_title(); ?></a>
-                            </h3>
-                            <p>
-                                <strong><?php if (osc_price_enabled_at_items()) { echo osc_item_formated_price(); ?> - <?php } echo osc_item_city(); ?> (<?php echo osc_item_region(); ?>) - <?php echo osc_format_date(osc_item_pub_date()); ?></strong>
-                            </p>
-                            <p><?php echo osc_highlight(strip_tags(osc_item_description())); ?></p>
-                            <p align="right"><a class="delete" onclick="javascript:return confirm('<?php _e('This action can not be undone. Are you sure you want to continue?', 'watchlist'); ?>')" href="<?php echo osc_render_file_url(osc_plugin_folder(__FILE__) . 'watchlist.php') . '?delete=' . osc_item_id(); ?>" ><?php _e('Delete', 'watchlist'); ?></a><p>
-                        </td>
-                    </tr>
-                    <?php $class = ($class == 'even') ? 'odd' : 'even'; ?>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-        <div class="paginate">
-            <?php echo osc_pagination(array('url' => osc_render_file_url(osc_plugin_folder(__FILE__) . 'watchlist.php') . '?iPage={PAGE}')); ?>
-        </div>
-        <?php } ?>
-    </div>
-</div>
+<?php 
+$adminoptions = false;
+osc_current_web_theme_path('head.php') ; ?>
+<body>
+	<?php osc_current_web_theme_path('header.php') ; ?>
+	<div class="panel panel-success row">
+		<div class="panel-heading">
+			<strong><?php _e('Your watchlist', 'watchlist'); ?></strong>
+		</div>
+		<div class="content user_account panel-body">
+			<div id="sidebar" class="col-md-3">
+				<?php echo tfc_private_user_menu() ; ?>
+				<div class="user-dashboard-widget"><?php osc_show_widgets('user-dashboard'); ?></div>
+			</div>
+			<div class="col-md-9">
+				<legend><?php _e('Your watchlist items', 'tuffclassified'); ?></legend>
+				<?php if(osc_count_items() == 0) { ?>
+				<h3><?php _e('You don\'t have any items yet', 'watchlist'); ?></h3>
+				<?php } else { ?>
+				<?php while(osc_has_items()) { ?>
+				<div class="item col-md-12" >
+					<?php include tfc_path().'ad-loop.php';?>
+					<p align="right"><a class="delete btn btn-danger btn-xs" onclick="javascript:return confirm('<?php _e('This action can not be undone. Are you sure you want to continue?', 'watchlist'); ?>')" href="<?php echo osc_render_file_url(osc_plugin_folder(__FILE__) . 'watchlist.php') . '&delete=' . osc_item_id(); ?>" ><?php _e('Delete', 'watchlist'); ?></a></p>
+				</div>
+				<?php } ?>
+				<?php } ?>
+				<div class="pagination col-md-6 col-md-offset-5" >
+					<?php echo tfc_pagination_items(array('url' => osc_render_file_url(osc_plugin_folder(__FILE__) . 'watchlist.php') . '&iPage={PAGE}')); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php osc_current_web_theme_path('footer.php') ; ?>
+	<?php osc_show_flash_message() ; ?>
+	<?php osc_run_hook('footer'); ?>
+</body>
+</html>
